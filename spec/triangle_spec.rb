df643f88fe8b66d67e991b81608644c54e56509e
Hugo Vila <git@hugovila.nom.es>
2014-07-15 11:46:38 +0200

    first commit

---
+++ spec/triangle_spec.rb
@@ -0,0 +1,11 @@
+
+require './polygon'
+require './triangle'
+
+describe Polygon do
+  let(:triangle) { Triangle.new }
+
+  describe Triangle do
+    
+  end
+end
---
+++ triangle.rb
@@ -0,0 +1,7 @@
+
+class Triangle < Polygon
+
+  
+
+
+end
