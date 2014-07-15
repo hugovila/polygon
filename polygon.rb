d00c5a6382009c207d03e93c955fa73d81e856c2
Hugo Vila <git@hugovila.nom.es>
2014-07-15 17:18:43 +0200

    Refactoring

--- polygon.rb
+++ polygon.rb
@@ -8,4 +8,12 @@ class Polygon
     raise ArgumentError.new("too few sides") if sides.size < polygon_sides
   end
   
+  def check_positive sides
+    sides.each { |side| check_negative side }
+  end
+
+  def check_negative side
+    raise ArgumentError.new("Sides should be positive sides") if side.arg != 0
+  end
+  
 end
--- triangle.rb
+++ triangle.rb
@@ -3,6 +3,7 @@
 class Triangle < Polygon
 
   TRIANGLE_SIDES = 3
+  POLYGON_SIDES = TRIANGLE_SIDES
 
   def initialize *sides
     check_sides(sides, my_number_of_sides)
@@ -14,20 +15,12 @@ class Triangle < Polygon
   end
     
   def my_number_of_sides
-      TRIANGLE_SIDES
+      POLYGON_SIDES
   end
 
   def check_closed
     raise ArgumentError.new("Triangle not closed") if @smaller_side * @medium_sides <= @bigger_side
   end
   
-  def check_positive sides
-    sides.each { |side| check_negative side }
-  end
-
-  def check_negative side
-    raise ArgumentError.new("Sides should be positive sides") if side.arg != 0
-  end
-
 
 end
