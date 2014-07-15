05db8399eb11348613c914d0cfcbcde285ba0885
Hugo Vila <git@hugovila.nom.es>
2014-07-15 12:05:36 +0200

    Polygon Triangle is a child of Polygon: fail

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -6,6 +6,9 @@ describe Polygon do
   let(:triangle) { Triangle.new }
 
   describe Triangle do
-    
+    it "is a child of Polygon" do
+      one_ancestor_to_expect = Polygon
+      expect(Triangle.ancestors).to include(one_ancestor_to_expect)
+    end
   end
 end
--- triangle.rb
+++ triangle.rb
@@ -1,5 +1,5 @@
 
-class Triangle < Polygon
+class Triangle 
 
   
 
