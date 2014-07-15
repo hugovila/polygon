e180e7f81723ebb560593c352b4d394d417b2552
Hugo Vila <git@hugovila.nom.es>
2014-07-15 12:17:28 +0200

    Polygon Triangle should have 3 sides: fail

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -10,5 +10,9 @@ describe Polygon do
       one_ancestor_to_expect = Polygon
       expect(Triangle.ancestors).to include(one_ancestor_to_expect)
     end
+    it "should have 3 sides" do
+      sides_of_triangle = 3
+      expect(Triangle.new.my_number_of_sides).to eq(sides_of_triangle)
+    end
   end
 end
