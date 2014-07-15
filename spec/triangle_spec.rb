e087f366c989762b37c6fbf0f07f28c1c8b51d20
Hugo Vila <git@hugovila.nom.es>
2014-07-15 18:08:27 +0200

    Polygon Triangle should be return its own area: pass

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -32,11 +32,11 @@ describe Polygon do
       expect{ Triangle.new(2, 4, "lado") }.to raise_error(ArgumentError)
     end
     it "return its area" do
-      a = 4
+      a = 2
       b = 3
-      c = 3
+      c = 4
       area = 0.25 * Math.sqrt( (a+(b+c)) * (c-(a-b)) * (c+(a-b)) * (a+(b-c)) )
-      expect(Triangle.new(3,4,3).area).to eq(area)
+      expect(Triangle.new(a,b,c).area).to eq(area)
     end
   end
 end
--- triangle.rb
+++ triangle.rb
@@ -13,14 +13,21 @@ class Triangle < Polygon
     check_closed
     check_positive sides
   end
-    
+
+  def area
+    0.25 * Math.sqrt( (@bigger_side+(@medium_sides+@smaller_side)) * (@smaller_side-(@bigger_side-@medium_sides)) * (@smaller_side+(@bigger_side-@medium_sides)) * (@bigger_side+(@medium_sides-@smaller_side)) )
+  end
+
   def my_number_of_sides
       POLYGON_SIDES
   end
+
+  private
 
   def check_closed
     raise ArgumentError.new("Triangle not closed") if @smaller_side * @medium_sides <= @bigger_side
   end
   
+
 
 end
