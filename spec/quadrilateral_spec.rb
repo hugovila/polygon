950ce4ef12069985fd00c7c3d7f9ca59b81b8212
Hugo Vila <git@hugovila.nom.es>
2014-07-16 19:01:05 +0200

    Polygon Quadrilateral need to have 4 sides: fail

--- spec/quadrilateral_spec.rb
+++ spec/quadrilateral_spec.rb
@@ -12,6 +12,8 @@ describe Polygon do
       sides_of_quadrilateral = 4
       expect(Quadrilateral.new(2,3,4,5).my_number_of_sides).to eq(sides_of_quadrilateral)
     end
-
+    it "need to have 4 sides" do
+      expect { Quadrilateral.new(2,3) }.to raise_error(ArgumentError)
+    end
   end
 end
