8859595c1272f029e96c2d0bcd0a6015b7d5f233
Hugo Vila <git@hugovila.nom.es>
2014-07-16 18:46:53 +0200

    Polygon Quadrilateral should have 4 sides: pass

--- quadrilateral.rb
+++ quadrilateral.rb
@@ -2,6 +2,15 @@
 
 class Quadrilateral < Polygon
 
-  
+  QUADRILATERAL_SIDES = 4
+  POLYGON_SIDES = QUADRILATERAL_SIDES
+
+  def initialize *sides
+    @sides = sides
+  end
+
+  def my_number_of_sides
+    POLYGON_SIDES
+  end
 
 end
