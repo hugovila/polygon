aed7eaeb403df060e1d83a7f987e5ed67ea523ea
Hugo Vila <git@hugovila.nom.es>
2014-07-17 13:12:19 +0200

    Polygon Quadrilateral Square is a child of Polygon: fail

---
+++ spec/square_spec.rb
@@ -0,0 +1,19 @@
+require './polygon'
+require './quadrilateral'
+require './square'
+
+
+describe Polygon do
+
+  describe Quadrilateral do
+
+    describe Square do
+
+      it "is a child of Polygon" do
+        one_ancestor_to_expect = Polygon
+        expect(Square.ancestors).to include(one_ancestor_to_expect)
+      end
+
+    end
+  end
+end
