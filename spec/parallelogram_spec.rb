e1149b2665147389ef719b1339f7dcc1d1965c00
Hugo Vila <git@hugovila.nom.es>
2014-07-17 13:30:41 +0200

    Polygon Quadrilateral Parallelogram  is a child of Quadrilateral: pass

--- spec/parallelogram_spec.rb
+++ spec/parallelogram_spec.rb
@@ -13,7 +13,10 @@ describe Polygon do
         one_ancestor_to_expect = Polygon
         expect(Square.ancestors).to include(one_ancestor_to_expect)
       end
-      
+      it "is a child of Quadrilateral" do
+        one_ancestor_to_expect = Quadrilateral
+        expect(Square.ancestors).to include(one_ancestor_to_expect)
+      end
     end
   end
 end
