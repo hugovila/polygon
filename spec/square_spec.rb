f1b321a55dbf2881b50b6b6bd641869191cd84b6
Hugo Vila <git@hugovila.nom.es>
2014-07-18 11:43:38 +0200

    Polygon Quadrilateral Square is a child of Parallelogram also: pass

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -24,6 +24,37 @@ describe Polygon do
           one_ancestor_to_expect = Parallelogram
           expect(Square.ancestors).to include(one_ancestor_to_expect)
         end
+        it "have 1 sides and not raise_error" do
+          equals_sides = 3
+          
+          expect{ Square.new(equals_sides) }.not_to raise_error
+        end
+        it "can be created with one side" do
+          sides_of_square = 4
+          expect(Square.new(3).my_number_of_sides).to eq(sides_of_square)
+        end
+        it "need to have 1 sides" do
+          expect { Square.new(2,2) }.to raise_error(ArgumentError)
+        end
+        it "need to have 1 sides" do
+          expect { Square.new(2,3,6,5,4) }.to raise_error(ArgumentError)
+        end
+        it "sides should have positive numbers" do
+          expect{Square.new(-3)}.to raise_error(ArgumentError)
+          expect{Square.new(-3)}.to raise_error(ArgumentError)
+          expect{Square.new(-4)}.to raise_error(ArgumentError)
+        end
+        it "all sides should be numbers" do
+          expect{ Square.new("lado") }.to raise_error(ArgumentError)
+        end
+        it "returns its own perimeter" do
+          expect(Square.new(5).perimeter).to eq(20)
+        end
+        it "return its own area" do
+          base = 2
+          area = base * base
+          expect(Square.new(base).area).to eq(area)
+        end
       end
     end
   end
--- square.rb
+++ square.rb
@@ -1,4 +1,8 @@
 
-class Square < Quadrilateral
+class Square < Parallelogram
+
+  def initialize side
+    super side, side
+  end
 
 end
